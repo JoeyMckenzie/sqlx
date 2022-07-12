04d04e5f6d78589e3a406ffa2a599e4f93f58367
Joey McKenzie <joey.mckenzie27@gmail.com>
2022-07-12 15:08:19 -0700

    fix(examples): run formatter to fix build failures

--- examples/postgres/files/src/main.rs
+++ examples/postgres/files/src/main.rs
@@ -1,5 +1,5 @@
+use sqlx::{query_file, query_file_as, query_file_unchecked, FromRow, PgPool};
 use std::fmt::{Display, Formatter};
-use sqlx::{PgPool, FromRow, query_file, query_file_as, query_file_unchecked};
 
 #[derive(FromRow)]
 struct PostWithAuthorQuery {
@@ -12,18 +12,17 @@ struct PostWithAuthorQuery {
 
 impl Display for PostWithAuthorQuery {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, r#"
+        write!(
+            f,
+            r#"
             post_id: {},
             title: {},
             body: {},
             author_id: {},
             author_username: {}
         "#,
-            self.post_id,
-            self.title,
-            self.body,
-            self.author_id,
-            self.author_username)
+            self.post_id, self.title, self.body, self.author_id, self.author_username
+        )
     }
 }
 
@@ -32,10 +31,14 @@ async fn main() -> anyhow::Result<()> {
     let pool = PgPool::connect(&dotenv::var("DATABASE_URL")?).await?;
 
     // we can use a tranditional wrapper around the `query!()` macro using files
-    query_file!("queries/insert_seed_data.sql").execute(&pool).await?;
+    query_file!("queries/insert_seed_data.sql")
+        .execute(&pool)
+        .await?;
 
     // we can also use `query_file_as!()` similarly to `query_as!()` to map our database models
-    let posts_with_authors = query_file_as!(PostWithAuthorQuery, "queries/list_all_posts.sql").fetch_all(&pool).await?;
+    let posts_with_authors = query_file_as!(PostWithAuthorQuery, "queries/list_all_posts.sql")
+        .fetch_all(&pool)
+        .await?;
 
     for post_with_author in posts_with_authors {
         println!("{}", post_with_author);
